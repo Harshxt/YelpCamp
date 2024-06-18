f212ce3dedb2de622fd3919a052599f3989cb4cf
Harshit Gupta <79516676+Harshxt@users.noreply.github.com>
2024-06-18 16:46:54 +0530

    Added Authorization and controller refctoring

--- seeds/index.js
+++ seeds/index.js
@@ -2,25 +2,26 @@
 const cities = require('./cities')
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () =>{
+db.once("open", () => {
     console.log("Database connected!")
 });
 
-const sample = (arr) => arr[Math.floor(Math.random()* arr.length)];
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-const seedDB = async() =>{
+const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i=0;i<50;i++){
-        const random1000 = Math.floor(Math.random()*1000);
-        const price = Math.floor(Math.random()*20) +10;
-       const camp = new Campground({
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '66711ad62c911cd1c2a76711',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://images.unsplash.com/photo-1505635725851-c2cfe9e29112',
@@ -32,6 +33,6 @@ const seedDB = async() =>{
     }
 }
 
-seedDB().then(()=>{
+seedDB().then(() => {
     mongoose.connection.close();
 })
