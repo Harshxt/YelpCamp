89190ca43a72c9f79458485458aa950a0e6e1da7
Harshit <79516676+Harshxt@users.noreply.github.com>
2025-10-16 17:27:19 +0530

    Fixed failed-login issue

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,8 @@ router.route('/register')
     .post(catchAsync(users.register));
 
 router.route('/login')
-    .get( users.renderLogin)
-    .post( storeReturnTo, passport.authenticate('local', { failureFlash: false, failureRedirect: '/login' }), users.login)
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout);
 
